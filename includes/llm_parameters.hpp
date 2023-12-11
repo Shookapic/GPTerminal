32e90de795de4f32c0e6db7c91262b70ccdad8cd
solo <cedric.roulof@epitech.eu>
2023-12-12 01:14:07 +0400

    no more crashing if the env variable is NULL

--- includes/llm_parameters.hpp
+++ includes/llm_parameters.hpp
@@ -13,14 +13,20 @@ class Parameters
         std::string base_url = "https://api.openai.com/v1/chat/completions";
         std::string model = "gpt-3.5-turbo";
         std::string user_input = "Hello !";
-        std::string response = "Initial Response"; //Do not change or erase the value.
+        std::string response = "Initial Response";
         std::string system_prompt = "You are a helpful assistant that answer to every questions. Answer with the same language of the question.";
 
-    Parameters() {
-        OPENAI_API_KEY = std::getenv("OPENAI_API_KEY");
-        if (OPENAI_API_KEY.empty()) {
-            std::cout << "Please set the OPENAI_API_KEY env variable (--help).";
-            exit(1);
+    Parameters()
+    {
+        const char *env_key = "OPENAI_API_KEY";
+        const char *env_value = std::getenv(env_key);
+
+        if (env_value == nullptr || env_value[0] == '\0')
+        {
+            std::cerr << "Error: Please set the " << env_key << " environment variable (--help).\n" << std::endl;
+            throw std::runtime_error("Missing OPENAI_API_KEY");
         }
+
+        OPENAI_API_KEY = env_value;
     }
 };
